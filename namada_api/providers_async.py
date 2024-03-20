058408bb1fbb0f2ab1c9363190601bd3acc315d5
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-03-20 18:42:24 +0800

    Update providers_async.py

--- namada_api/providers_async.py
+++ namada_api/providers_async.py
@@ -215,22 +215,4 @@ class NamadaProvider:
         return result
 
 
-# async def main():
-#     provider = NamadaProvider("https://namada-testnet-rpc.blackoreo.xyz")
-#     latest_height_result = await provider.get_latest_height()
-#     if latest_height_result.success:
-#         print("Latest height:", latest_height_result.data)
-#     else:
-#         print("Error fetching latest height:", latest_height_result.error)
-#
-#     resp = await provider.get_epoch()
-#     epoch = resp.data
-#     print(epoch)
-#
-#     resp = await provider.get_proposal_detail(371, epoch)
-#     print(resp.data)
-#     await provider.close()
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
+
